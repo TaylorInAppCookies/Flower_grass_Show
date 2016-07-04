8d20993a6615e068c046b50f2789a0abcf892aa6
Taylor <tank357@icloud.com>
2016-07-04 18:38:49 +0800

    修改order模型，增加简单创建订单逻辑

--- escort/model/order.py
+++ escort/model/order.py
@@ -7,8 +7,8 @@ from model.base import Base
 
 class Order(Base):
     __tablename__ = 'Order'
-    progress_set = {'on', 'off', 'completed'}
-
+    progress_set = {'on', 'attemper', 'run', 'abandon', 'deleted', 'paid',
+                    'arrive', 'sign', 'complete', 'off', }
     id = Column(Integer, primary_key=True)
     title = Column(String(140))
     describe = Column(String(800))
@@ -18,15 +18,9 @@ class Order(Base):
     location_x = Column(Float)
     location_y = Column(Float)
     progress = Column(Enum(progress_set, name='progress'))
-    is_complete = Column(Boolean)
-    is_paid = Column(Boolean)
-    is_abandon = Column(Boolean)
-    is_running = Column(Boolean)
-    is_deleted = Column(Boolean)
 
     def __init__(self, title=None, describe=None, money=None, send_time=None,
-                 paid_at=None, location_x=None, location_y=None, is_complete=None,
-                 is_paid=None, is_abandon=None, is_running=None, is_deleted=None):
+                 paid_at=None, location_x=None, location_y=None, progress='off'):
         self.title = title
         self.describe = describe
         self.money = money
@@ -34,12 +28,4 @@ class Order(Base):
         self.paid_at = paid_at
         self.location_x = location_x
         self.location_y = location_y
-        self.is_complete = is_complete
-        self.is_paid = is_paid
-        self.is_abandon = is_abandon
-        self.is_running = is_running
-        self.is_deleted = is_deleted
-
-    def json(self):
-        pass
-        # TODO add return messages of Order using json
+        self.progress = progress
--- escort/view/escort.py
+++ escort/view/escort.py
@@ -1,6 +1,15 @@
-from flask import Flask
+# coding: utf-8
+from flask import Flask, request, render_template
+from model.base import db_session
+
+from model.order import Order
 
 app = Flask(__name__)
+
+
+@app.before_request
+def pre():
+    pass
 
 
 @app.route('/')
@@ -8,5 +17,26 @@ def hello_world():
     return 'Hello escort!'
 
 
+@app.route('/make_a_order', methods=['POST', 'GET'])
+def make_a_order():
+    if request.method == 'POST':
+        # 从请求表单中创建变量
+        title = request.form['title']
+        describe = request.form['describe']
+        money = request.form['money']
+        send_time = request.form['send_time']
+        paid_at = request.form['paid_at']
+        location_x = request.form['location_x']
+        location_y = request.form['location_y']
+        progress = Order.progress_set['on']
+        # 简单的订单创建逻辑,没有考虑恶意刷单,用户验证情况
+        order = Order(title, describe, money, send_time,
+                      paid_at, location_x, location_y, progress)
+        db_session.add(order)
+        db_session.commit()
+        return render_template('result_create_order.html', result=True)
+    if request.method == 'GET':
+        return render_template('create_order.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

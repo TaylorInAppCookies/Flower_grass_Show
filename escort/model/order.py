368ac4f95e2f9131a7fbd3286b8eb2b8a4701331
Taylor <tank357@icloud.com>
2016-07-05 00:23:20 +0800

    创建订单模块更新

--- escort/model/order.py
+++ escort/model/order.py
@@ -1,30 +1,33 @@
 # coding: utf-8
+import datetime
 from sqlalchemy import Column, Integer, String, Text, Boolean, Float, DateTime, Enum
 from sqlalchemy.orm import relationship
 
 from model.base import Base
 
 
+class Progeress_Enum():
+    on = 'on'
+    attemper = 'attemper'
+    run = 'run'
+    abandon = 'abandon'
+    deleted = 'deleted'
+    paid = 'paid'
+    arrive = 'arrive'
+    sign = 'sign'
+    complete = 'complete'
+    off = 'off'
+
+
 class Order(Base):
     __tablename__ = 'Order'
-
-    class Progeress_Enum():
-        on = 'on'
-        attemper = 'attemper'
-        run = 'run'
-        abandon = 'abandon'
-        deleted = 'deleted'
-        paid = 'paid'
-        arrive = 'arrive'
-        sign = 'sign'
-        complete = 'complete'
-        off = 'off'
 
     id = Column(Integer, primary_key=True)
     title = Column(String(140))
     describe = Column(String(800))
     money = Column(Float)
-    send_time = Column(DateTime)
+    create_at = Column(DateTime)
+    send_at = Column(DateTime)
     paid_at = Column(DateTime)
     location_x = Column(Float)
     location_y = Column(Float)
@@ -32,11 +35,13 @@ class Order(Base):
                       Enum('on', 'attemper', 'run', 'abandon', 'deleted',
                            'paid', 'arrive', 'sign', 'complete', 'off'))
 
-    def __init__(self, title=None, describe=None, money=None, send_time=None,
-                 paid_at=None, location_x=None, location_y=None, progress='off'):
+    def __init__(self, title=None, describe=None, money=None, create_at=None,
+                 send_time=None, paid_at=None, location_x=None,
+                 location_y=None, progress=Progeress_Enum.off):
         self.title = title
         self.describe = describe
         self.money = money
+        self.create_at = create_at
         self.send_time = send_time
         self.paid_at = paid_at
         self.location_x = location_x
--- escort/view/escort.py
+++ escort/view/escort.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import datetime
 from flask import Flask, request, render_template
 from model.base import db_session
 
@@ -19,26 +20,31 @@ def hello_world():
 
 @app.route('/make_a_order', methods=['POST', 'GET'])
 def make_a_order():
+    """"
+    version:0.0.1
+    创建一个订单,必须的字段
+    title,describe,money,location_x,location_y
+    """
+
     if request.method == 'POST':
         # 从请求表单中创建变量
         title = request.form['title']
         describe = request.form['describe']
         money = request.form['money']
-        # 需要对时间相关的字段做处理
-        send_time = request.form['send_time']
-        paid_at = request.form['paid_at']
-        #
         location_x = request.form['location_x']
         location_y = request.form['location_y']
         progress = Order.Progeress_Enum.on
         # 简单的订单创建逻辑,没有考虑恶意刷单,用户验证情况
-        order = Order(title, describe, money, send_time,
-                      paid_at, location_x, location_y, progress)
+        order = Order(title=title, describe=describe, money=money,
+                      create_at=datetime.datetime.now(),
+                      send_time=None, paid_at=None,
+                      location_x=location_x, location_y=location_y, progress=progress)
         db_session.add(order)
         db_session.commit()
         return render_template('result_create_order.html')
     if request.method == 'GET':
         return render_template('create_order.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
